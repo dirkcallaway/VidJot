7e16665e2a93ef08baca92c82bf5e4b5d581693a
dirkcallaway <35826733+dirkcallaway@users.noreply.github.com>
2019-06-30 16:11:25 -0600

    Added user registration with hashing

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+// const passport = require('passport');
+
+//load User Model
+require('../models/User');
+const User = mongoose.model('users');
 
 // User Login Route
 router.get('/login', (req, res) => {
@@ -11,5 +17,62 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
+
+//Register Form POST
+router.post('/register', (req, res) => {
+    let errors = [];
+    if (req.body.password != req.body.password2) {
+        errors.push({
+            text: "Passwords don't match."
+        })
+    }
+
+    if (req.body.password.length < 4) {
+        errors.push({
+            text: 'Password must be at least 4 characters.'
+        });
+    }
+
+    if (errors.length > 0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        });
+    } else {
+        User.findOne({
+                email: req.body.email
+            })
+            .then(user => {
+                if (user) {
+                    req.flash('error_msg', 'That email is already in use.')
+                    res.redirect('/users/register')
+                } else {
+                    const newUser = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    })
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw (err);
+                            newUser.password = hash;
+                            newUser.save()
+                                .then(user => {
+                                    req.flash('success_msg', 'You are now registered!');
+                                    res.redirect('/users/login');
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                })
+                        });
+                    });
+                }
+            })
+    }
+})
 
 module.exports = router;
