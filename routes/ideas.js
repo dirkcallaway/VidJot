d059d5ab0c3079ae2204d1dec38dd79e648b7ce8
dirkcallaway <35826733+dirkcallaway@users.noreply.github.com>
2019-06-30 20:56:47 -0600

    Added Access Control

--- routes/ideas.js
+++ routes/ideas.js
@@ -9,7 +9,9 @@ const Idea = mongoose.model('ideas');
 
 // Idea Index Page
 router.get('/',ensureAuthenticated, (req, res) => {
-    Idea.find({})
+    Idea.find({
+        user: req.user.id
+    })
     .sort({date: 'desc'})
     .then(ideas => {
         res.render('ideas/index', {
@@ -29,9 +31,14 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
         _id: req.params.id
     })
     .then(idea => {
+        if(idea.user != req.user.id){
+            req.flash('error_msg', 'Not Authorized')
+            res.redirect('/ideas');
+        }else{
         res.render('ideas/edit', {
             idea: idea
         });
+    }
     })
 });
 
@@ -54,7 +61,8 @@ router.post('/', ensureAuthenticated, (req, res) => {
     } else {
         const newUser = {
             title: req.body.title,
-            details: req.body.details
+            details: req.body.details,
+            user: req.user.id
         }
         new Idea(newUser)
         .save()
